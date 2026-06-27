eb9419f6c089fa1fd20391bf604223f775d7dcea
henry6180 <henrylin618@gmail.com>
2026-06-27 14:10:16 +0000

    feat: add options to kfetch.c for power info

--- kfetch.h
+++ kfetch.h
@@ -9,16 +9,18 @@
 
 #define KFETCH_DEV_NAME "kfetch"
 #define KFETCH_DEV_PATH "/dev/kfetch"
-#define KFETCH_BUF_SIZE 1024
+#define KFETCH_BUF_SIZE 2048
 
-#define KFETCH_NUM_INFO 6
+#define KFETCH_NUM_INFO 8
 
-#define KFETCH_RELEASE   (1 << 0)
-#define KFETCH_NUM_CPUS  (1 << 1)
-#define KFETCH_CPU_MODEL (1 << 2)
-#define KFETCH_MEM       (1 << 3)
-#define KFETCH_UPTIME    (1 << 4)
-#define KFETCH_NUM_PROCS (1 << 5)
+#define KFETCH_RELEASE      (1 << 0)
+#define KFETCH_NUM_CPUS     (1 << 1)
+#define KFETCH_CPU_MODEL    (1 << 2)
+#define KFETCH_MEM          (1 << 3)
+#define KFETCH_UPTIME       (1 << 4)
+#define KFETCH_NUM_PROCS    (1 << 5)
+#define KFETCH_CPU_POWER    (1 << 6)
+#define KFETCH_PROC_POWER   (1 << 7)
 
 #define KFETCH_FULL_INFO ((1 << KFETCH_NUM_INFO) - 1);
 
